b023d361253907fd694e3a42a897fa794f5149d0
Adam Darby <kyds3k@gmail.com>
2020-08-16 20:51:09 -1000

    Now working using dummy data and original StudentTableRow.js

--- src/components/StudentList.js
+++ src/components/StudentList.js
@@ -6,6 +6,107 @@ import StudentTableRow from './StudentTableRow';
 const StudentList = () => {
   const [students, setStudents] = useState([]);
 
+  const studentArray = [
+    {
+      _id: '5f36dedf1286d95965005d80',
+      name: 'Adam Darby',
+      email: 'kyds3k@gmail.com',
+      rollno: 666,
+      __v: 0
+    },
+    {
+      _id: '5f36def41286d95965005d81',
+      name: 'Sarah-Blythe Ballard',
+      email: 'thenavydoc@gmail.com',
+      rollno: 777,
+      __v: 0
+    },
+    {
+      _id: '5f391a7cd916f55a3023c3c5',
+      name: 'Adam Darby',
+      email: 'kyds3k@gmail.com',
+      rollno: 666,
+      __v: 0
+    },
+    {
+      _id: '5f391f1fd916f55a3023c3c6',
+      name: 'Adam Darby',
+      email: 'kyds3k@gmai.com',
+      rollno: 566,
+      __v: 0
+    },
+    {
+      _id: '5f392026d916f55a3023c3c7',
+      name: 'Adam Darby',
+      email: 'kyds3k@gmail.com',
+      rollno: 666,
+      __v: 0
+    },
+    {
+      _id: '5f392062d916f55a3023c3c8',
+      name: 'Adam Darby',
+      email: 'kyds3k@gmail.com',
+      rollno: 666,
+      __v: 0
+    },
+    {
+      _id: '5f39207ad916f55a3023c3c9',
+      name: 'Adam Darby',
+      email: 'kyds3k@gmail.com',
+      rollno: 666,
+      __v: 0
+    },
+    {
+      _id: '5f3920a5d916f55a3023c3ca',
+      name: 'Adam Darby',
+      email: 'kyds3k@gmail.com',
+      rollno: 2252,
+      __v: 0
+    },
+    {
+      _id: '5f3920d7d916f55a3023c3cb',
+      name: 'Adam Darby',
+      email: 'kyds3k@gmail.com',
+      rollno: 2424,
+      __v: 0
+    },
+    {
+      _id: '5f3921d3d916f55a3023c3cc',
+      name: 'Adam Darby',
+      email: 'kyds3k@gmail.com',
+      rollno: 444,
+      __v: 0
+    },
+    {
+      _id: '5f3921d8d916f55a3023c3cd',
+      name: 'Adam Darby',
+      email: 'kyds3k@gmail.com',
+      rollno: 444,
+      __v: 0
+    },
+    {
+      _id: '5f39241b05292663cb2f13bf',
+      name: 'Adam Darby',
+      email: 'kyds3k@gmail.com',
+      rollno: 444,
+      __v: 0
+    },
+    {
+      _id: '5f3924f605292663cb2f13c0',
+      name: 'Adam Darby',
+      email: 'kyds3k@gmail.com',
+      rollno: 4242,
+      __v: 0
+    },
+    {
+      _id: '5f39261405292663cb2f13c1',
+      name: 'Adam Darby',
+      email: 'kyds3k@gmail.com',
+      rollno: 242,
+      __v: 0
+    }
+  ];
+
   useEffect(() => {
     axios
       .get('http://localhost:4000/students/')
@@ -13,19 +114,17 @@ const StudentList = () => {
         setStudents({
           students: res.data
         });
+        //console.log(students);
       })
       .catch(error => {
         console.log(error);
       });
   }, []); //notice the empty array here
 
-  // const DataTable = students => {
-  //   setTimeout(() => {
-  //     console.log(students.students);
-  //     // return students[0].map(kid => {
-  //     //   return <td key={kid._id}>{kid.name}</td>;
-  //     // });
-  //   }, 2000);
+  // const DataTable = data => {
+  //   return data.map(el => {
+  //     return <StudentTableRow obj={el} key={el._id} />;
+  //   });
   // };
 
   return (
@@ -40,8 +139,8 @@ const StudentList = () => {
           </tr>
         </thead>
         <tbody>
-          {students.map(student => {
-            return <StudentTableRow obj={student} key={student.id} />;
+          {studentArray.map(student => {
+            return <StudentTableRow obj={student} key={student._id} />;
           })}
         </tbody>
       </Table>
--- src/components/StudentTableRow.js
+++ src/components/StudentTableRow.js
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 
-const StudentTableRow = props => {
-  return (
-    <tr>
-      <td>{props.name}</td>
-      <td>{props.email}</td>
-      <td>{props.rollno}</td>
-      <td>
-        <Link className='edit-link' to={'/edit-student/' + props._id}>
-          Edit
-        </Link>
-        <Button size='sm' variant='danger'>
-          Delete
-        </Button>
-      </td>
-    </tr>
-  );
-};
-
-export default StudentTableRow;
+export default class StudentTableRow extends Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.obj.name}</td>
+        <td>{this.props.obj.email}</td>
+        <td>{this.props.obj.rollno}</td>
+        <td>
+          <Link
+            className='edit-link'
+            to={'/edit-student/' + this.props.obj._id}>
+            Edit
+          </Link>
+          <Button size='sm' variant='danger'>
+            Delete
+          </Button>
+        </td>
+      </tr>
+    );
+  }
+}
